06d3eba0ff8fc5d67b3213cba4e470ddc6bc4b23
coolguy967 <abdurrahman.ansari@uoit.net>
2018-11-17 15:03:38 -0500

    Initialized required activities

--- app/src/main/java/ca/uoit/project/DBHelper.java
+++ app/src/main/java/ca/uoit/project/DBHelper.java
@@ -6,10 +6,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper
 {
-    private static final String DATABASE_NAME = "";
+    private static final String DATABASE_NAME = "d";
     private static final Integer DATABASE_VERSION = 1;
-    private static final String TABLE_NAME = "";
+    private static final String TABLE_NAME = "d";
     private static final String COL_1 = "id";
+    private static final String COL_2 = "";
+    private static final String COL_3 = "";
+    private static final String COL_4 = "";
+    private static final String COL_5 = "";
+    private static final String COL_6 = "";
 
     public DBHelper(Context context)
     {
@@ -28,6 +33,7 @@ public class DBHelper extends SQLiteOpenHelper
     @Override
     public void onUpgrade(SQLiteDatabase db, int old_version, int new_version)
     {
-
+        db.execSQL("Drop Table " + TABLE_NAME + ";");
+        this.onCreate(db);
     }
 }
